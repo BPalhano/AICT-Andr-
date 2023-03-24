8f4684241386606dc803810483bc8eefde7cba74
BPalhano <igor.palhano@alu.ufc.br>
2023-03-23 22:36:18 -0300

    second homework scketch

--- BER_experiment/nmse.m
+++ BER_experiment/nmse.m
@@ -1,12 +1,14 @@
-function [plot] = nmse(tensor_hat, tensor, EbN0_range)
+function [graph] = nmse(tensor_hat, tensor, EbN0_range)
     
     NMSE = zeros(size(tensor_hat, 1), size(tensor_hat, 2));
 
-    for i = i:size(tensor,2)
+    for i = 1:size(tensor,2)
         
-        
+        NMSE(i,:) = ((norm(tensor(i,:) - tensor_hat(i,:), "fro")) ...
+            / (norm(tensor(i,:), "fro"))^2);
     end
 
+    graph = plot(EbN0_range, NMSE, "Marker","diamond");
 
 end
 
