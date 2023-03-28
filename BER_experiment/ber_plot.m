7df4c921f896bc6354fdc915022c779bdd61fae5
BPalhano <igor.palhano@alu.ufc.br>
2023-03-28 16:45:22 -0300

    plot fix

--- BER_experiment/ber_plot.m
+++ BER_experiment/ber_plot.m
@@ -1,15 +1,23 @@
 function [graph] = ber_plot(estimate, real)
 
     N = size(estimate,1);
-    error = zeros(N,1);
+    error = zeros(N/100,1);
+    MC_error = zeros(100,1);
+    counter = 1;
 
     for i = 1:N
 
-        error(i) = biterr(estimate(i,:), real) / 16;
+        MC_error(i) = (biterr(estimate(i,:), real)/ size(estimate, 2));
+        if mod(i,100) == 0
+            
+            error(counter) = mean(MC_error(:));
+            MC_error = zeros(100,1);
+            counter = counter + 1;
+        end
     end
 
 
-    graph = plot(-20:20, error, 'Color', 'blue');
+    graph = plot(linspace(-20,20, N/100), error, 'Color', 'blue');
     hold on
     xlabel("Eb/N0");
     ylabel("BER");
--- BER_experiment/simple_ber.m
+++ BER_experiment/simple_ber.m
@@ -23,24 +23,30 @@ C_8x1 = kron(A_8x1,B_8x1);
 
 plot_8x1 = [];
 
+realization = 100;
+
 % Here I have to do a Monte-Carlo simulation.
 for EbN0 = -20:20 % SNR range (dB)
-    
-    i = 10^(EbN0/10);
-    sigma = norm(C_8x1, "fro") / (i * norm(n,"fro"));    
-    Noise = rand(size(C_8x1,1), size(C_8x1,2));
-    
-    y_8x1 = C_8x1 + sigma*Noise;
-    [B_8x1_hat, A_8x1_hat] = norm_lskf(reshape(y_8x1, [8 8]), 1);
-    
-    A_8x1_hat = bpsk_decider(A_8x1_hat, 0);
-    B_8x1_hat = bpsk_decider(B_8x1_hat, 0);
 
-    plot_8x1 = [plot_8x1; A_8x1_hat', B_8x1_hat'];
+    sigma = 1/(10^(EbN0/10)); 
+    % Noise = rand(size(C_8x1,1), size(C_8x1,2));
+    % sigma = norm(C_8x1, "fro") / (i * norm(Noise,"fro"));
+
+    for ii = 1:realization
+        
+        noise = sqrt(sigma) * sqrt(1/2) * (randn(size(C_8x1)) + 1i*randn(size(C_8x1)));
+        y_8x1 = C_8x1 + noise;
+        [B_8x1_hat, A_8x1_hat] = norm_lskf(reshape(y_8x1, [n n]), 1);
+    
+        A_8x1_hat = bpsk_decider(A_8x1_hat, 0);
+        B_8x1_hat = bpsk_decider(B_8x1_hat, 0);
+
+        plot_8x1 = [plot_8x1; A_8x1_hat', B_8x1_hat'];
+    end
     
 end
 
-nmse(plot_8x1, s)
+ber_plot(plot_8x1, s)
 
 
  
