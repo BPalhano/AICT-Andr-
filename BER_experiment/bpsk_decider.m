5a00e6cf99b4b0c4ae6e9df6b7985f8e4872b420
BPalhano <igor.palhano@alu.ufc.br>
2023-04-05 11:34:49 -0300

    bpsk_decider fix, legend

--- BER_experiment/bpsk_decider.m
+++ BER_experiment/bpsk_decider.m
@@ -1,17 +1,6 @@
 function symbols_hat = bpsk_decider(symbols, filter)
 
-symbols_hat = zeros(size(symbols,1), size(symbols,2));
-
-for i = 1:size(symbols, 1)
-
-    if(symbols(i) < filter)
-        symbols_hat(i) = 0;
-
-    else
-        symbols_hat(i) = 1;
-    end
-end
-
+symbols_hat = (symbols > filter);
 
 end
 
