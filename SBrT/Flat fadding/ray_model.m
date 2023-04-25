78bb7c3e6a39b77c3d76fa27d700ad18498e2456
BPalhano <igor.palhano@alu.ufc.br>
2023-04-25 20:14:39 -0300

    sbrt folder removal

--- SBrT/Flat fadding/ray_model.m
+++
@@ -1,8 +0,0 @@
-function FF = ray_model(L)
-% Rayleigh Channel Model
-% Inputs:
-% L : Number of Channel Realizations
-% Outputs:
-% FF = Channel fast-term fadding vector
-
-FF = (randn(1,L) +1j*randn(1,L)/sqrt(2));
